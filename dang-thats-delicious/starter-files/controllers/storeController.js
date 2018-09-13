339d34621b2a3a4f22c37a8c523591a68a2ccb48
ben-dalton <benadalton@gmail.com>
2018-09-13 12:31:28 -0400

    backup wesbos courses

---
+++ dang-thats-delicious/starter-files/controllers/storeController.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const Store = mongoose.model('Store')
+
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Ben'
+  next()
+}
+
+exports.homePage = (req, res) => {
+  res.render('index')
+}
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' })
+}
+
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save()
+  req.flash(
+    'success',
+    `Successfully created ${store.name}! Care to leave a review?`
+  )
+  res.redirect(`/store/${store.slug}`)
+}
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find()
+  res.render('stores', { title: 'Stores', stores })
+}
+
+exports.editStore = async (req, res) => {
+  const store = await Store.findOne({ _id: req.params.id })
+  // TODO: confirm user ownership of store
+  res.render('editStore', { title: `Edit ${store.name}`, store })
+}
+
+exports.updateStore = async (req, res) => {
+  const query = { _id: req.params.id }
+  const options = { new: true, runValidators: true }
+  const store = await Store.findOneAndUpdate(query, req.body, options).exec()
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="stores/${store.slug}">View Store</a>`
+  )
+  res.redirect(`/stores/${store._id}/edit`)
+}
